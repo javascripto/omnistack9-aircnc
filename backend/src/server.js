382099ffb6b5de800fa290561662b31f8eada2cd
Yuri Alves <yurialves2@gmail.com>
2019-10-05 14:42:06 -0300

    database and model configuration

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,21 @@
+const path = require('path');
+const dotenv = require('dotenv');
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 
 
+dotenv.config({ path: path.join(__dirname, '..', '.env') });
+
 const app = express();
+
+mongoose.connect(process.env.MONGO_CONNECTION, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 app.use(express.json());
 app.use(routes);
 
 
-app.listen(3333);
+app.listen(process.env.PORT || 3333);
